f4c2febd7a374b8b251fd3214e691dad80174e07
Susmita Sen <99035356+sus08@users.noreply.github.com>
2022-02-04 21:30:05 +0530

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
-    Plugin Name: Simple Social Media Addon
-    Description: This is a simple social media plugin created for fun.
+    Plugin Name: Simple plugin
+    Description: This is a simple description.
     Author: Susmita Sen
 
 
@@ -9,12 +9,11 @@
 add_action( 'admin_menu', 'register_my_custom_menu_page' );
 function register_my_custom_menu_page() {
   // add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
-  add_menu_page( 'Social Media Addon', 'Sunny Leone', 'manage_options', 'sma_menu', 'sma_func', 'dashicons-share', 7 );
+  add_menu_page( 'Simple plugin', 'Simple plugin', 'manage_options', 'sma_menu', 'sma_func', 'dashicons-share', 7 );
 }
 function sma_func(){
     ?>
-<img src="https://stat1.bollywoodhungama.in/wp-content/uploads/2016/03/54649565.jpg">
-
+<!--write what you want-->
 <?php
 }
 ?>
